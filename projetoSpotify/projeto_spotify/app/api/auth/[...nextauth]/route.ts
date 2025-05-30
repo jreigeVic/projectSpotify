340c24640f1a2e6f0a4f0f59e1ef27436a314461
JreigeVic <jreigevictor@gmail.com>
2025-05-29 22:52:48 -0300

    solve build issues

--- projetoSpotify/projeto_spotify/app/api/auth/[...nextauth]/route.ts
+++ projetoSpotify/projeto_spotify/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,5 @@
 import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
-
-export const authOptions = {
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID!,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-      authorization: "https://accounts.spotify.com/authorize?scope=user-read-email,user-read-private,user-top-read",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET, // ⚠️ IMPORTANTE!
-  pages: {
-    signIn: "/",
-  },
-  callbacks: {
-    async jwt({ token, account }: { token: any; account: any }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-};
+import authOptions from "../../../../lib/authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- projetoSpotify/projeto_spotify/app/api/users/index.ts
+++ projetoSpotify/projeto_spotify/app/api/users/index.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import pool from '../../db';
+// import pool from '../../../db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
-            const { rows } = await pool.query('SELECT * FROM users');
-            res.status(200).json(rows);
+            // const { rows } = await pool.query('SELECT * FROM users');
+            // res.status(200).json(rows);
         } catch (error) {
             res.status(500).json({ error: 'Erro ao buscar usuários' });
         }
